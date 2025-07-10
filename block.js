90f56b471cb95195b0e49da98600cb397fba14cb
Amougou Silvere <138263116+silvereamougou@users.noreply.github.com>
2025-07-10 15:16:52 +0200

    Update block.js

--- block.js
+++ block.js
@@ -1,3 +1,7 @@
 import { decorate as decorateTestaccordion } from './blocks/test-accordion/test-accordion.js';
 
 window.hlx.blocks.set('accordion', decorateTestaccordion);
+
+import { decorate as decoratetest } from './blocks/test/test.js';
+
+window.hlx.blocks.set('test', decoratetest);
