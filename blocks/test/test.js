e4da2ccca6fabf1b156090f58f205563c3c35a3d
Amougou Silvere <138263116+silvereamougou@users.noreply.github.com>
2025-07-10 13:56:08 +0200

    Update test.js

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -1,6 +1,28 @@
 export default function decorate(block) {
-  const message = document.createElement('p');
-  message.textContent = 'Hello from the test block!';
-  block.appendChild(message);
+  const [title, description] = block.children;
+
+  const section = document.createElement('section');
+  section.className = 'test';
+
+  if (title) {
+    const h1 = document.createElement('h1');
+    h1.textContent = title.textContent;
+    section.appendChild(h1);
+  }
+
+  if (description) {
+    const p = document.createElement('p');
+    p.textContent = description.textContent;
+    section.appendChild(p);
+  }
+//adding image to the hero('Yes i am using a hero')
+ const imgCell = block.children[2];
+if (imgCell) {
+  const img = imgCell.querySelector('img');
+  if (img) section.appendChild(img);
+}
+
+  block.innerHTML = '';
+  block.appendChild(section);
 }
 
